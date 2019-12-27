4fa7d661667d6563396c9e04a5f180b3dae3bcb4
sudo-shuhei <aim54.tomneko.sud@gmail.com>
2019-12-27 21:22:06 +0900

    集計

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
   def top
+    @items = Item.all
+    @cost_month = 0
+    @items.each do |item|
+      puts item.date
+      if item.date.month == Date.today.month
+        # puts "今月"
+        @cost_month += item.price
+      end
+    end
   end
 end
