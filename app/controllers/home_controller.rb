87e5bc39214602f8094af05e8d59cbfcdb27dda5
sudo-shuhei <aim54.tomneko.sud@gmail.com>
2019-12-27 22:33:25 +0900

    pie_chart

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,20 @@
 class HomeController < ApplicationController
   def top
     @items = Item.all
+    @items_month = []
     @cost_month = 0
     @items.each do |item|
-      puts item.date
       if item.date.month == Date.today.month
-        # puts "今月"
+        @items_month.push(item)
         @cost_month += item.price
       end
+    end
+
+    @chart = {}
+    group = Item.group(:category).sum(:price)
+    group.each do |k,v|
+      category_name = Category.find_by(id: k).name
+      @chart[category_name] = v
     end
   end
 end
